f18ce84a3dbecf7ebba4450e35f942613d1c630f
Eirik Kvistad <eirik.kvistad@knowit.no>
2024-11-07 21:58:46 +0100

    up and running

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// src/index.js
+import "./styles.css";
+
+const root = document.createElement("div");
+root.innerHTML = "<h1>Hello, Webpack!</h1>";
+document.body.appendChild(root);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js", // Main entry file
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development", // Use 'production' for optimized builds
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Handle CSS files
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/, // Transpile JavaScript with Babel
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Cleans the 'dist' folder before each build
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Generates HTML file with the bundle
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // Specify the static directory
+    },
+    hot: true, // Enables hot module replacement
+  },
+
+  devtool: "inline-source-map", // Source maps for easier debugging
+};
